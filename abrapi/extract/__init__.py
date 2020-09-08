fefc711a93de10b882f069392e805d99e4674262
João Gabriel <jgsb123@gmail.com>
2020-09-08 15:23:03 -0300

    teste do __init__.py e melhoria no nome de algumas vars

--- abrapi/extract/__init__.py
+++ abrapi/extract/__init__.py
@@ -21,42 +21,39 @@ def resgatar_pedidos(data_inicio, data_fim):
                            "desc&f_creationDate=creationDate:%5B" + str(data_inicio) + \
                            "T03:00:00.000Z%20TO%20" + str(data_fim) + "T02:59:59" \
                                                                       ".999Z%5D&utc=-0200&per_page=100&page={}"
-    url_get_order_casa = "https://abramais.myvtex.com/api/oms/pvt/orders/{}"
+    url_get_order_casa = "https://abracasa.myvtex.com/api/oms/pvt/orders/{}"
     headers_casa = {
         "X-VTEX-API-AppKey": "vtexappkey-abracasa-ALPXOA",
         "X-VTEX-API-AppToken": "GPYPEPWLVPLINMRINWUNQFZBTWUZQVUUUJHAQUHEIQTIJCOIRRWKCMHBQWVHVXUZUQ"
-                               "NDFQRTOQFZLQRRHLHZZKSIBGKUYZIKWUNTOLDAZCWMHHPZFDAJPUAKYFBZGXUO"
-                               "PUMYJCOYMPETOLWNLWKHRLNOHHRMQNXUPGRLXXFGYFGTDITPGSUAMWYDGXRBLCIZSKE",
+                               "NDFQRTOQFZLQRRHLHZZKSIBGKUYZIKWUNTOLDAZCWMHHPZFDAJPUAKYFBZGXUO",
         "Content-type": "application/json"
     }
 
     # Abra mais
 
     # pegar todos os pedidos da api de list orders
-    extrator_list_mais = ListOrdersExtrator(url_list_orders_mais, headers_mais)
-    pedidos_mais = [pedidos_mais for pedidos_mais in extrator_list_mais]
+    abramais_list_extrator = ListOrdersExtrator(url_list_orders_mais, headers_mais)
+    pedidos_mais = [pedidos for pedidos in abramais_list_extrator]
 
     # filtrar os ids dos pedidos (cadabra e mais)
-    seletor_mais = Seletor(pedidos_mais)
-    ids_cadabra, ids_mais = seletor_mais.filtrar_ids_cadabra_mais()
+    ids_cadabra, ids_mais = Seletor().filtrar_ids_cadabra_mais(pedidos_mais)
 
     # pegar os pedidos (cadabra e mais) da api get orders
-    extrator_get_mais = Extrator(url_get_order_mais, headers_mais)
-    pedidos_cadabra = [extrator_get_mais.baixar_pagina(id_cadabra) for id_cadabra in ids_cadabra]
-    pedidos_mais = [extrator_get_mais.baixar_pagina(id_mais) for id_mais in ids_mais]
+    abramais_get_extrator = Extrator(url_get_order_mais, headers_mais)
+    pedidos_cadabra = [abramais_get_extrator.baixar_pagina(id_cadabra) for id_cadabra in ids_cadabra]
+    pedidos_mais = [abramais_get_extrator.baixar_pagina(id_mais) for id_mais in ids_mais]
 
     # Abra Casa
 
     # pegar todos os pedidos da api de list orders
-    extrator_list_casa = ListOrdersExtrator(url_list_orders_casa, headers_casa)
-    pedidos_casa = [pedidos_casa for pedidos_casa in extrator_list_casa]
+    abracasa_list_extrator = ListOrdersExtrator(url_list_orders_casa, headers_casa)
+    pedidos_casa = [pedidos for pedidos in abracasa_list_extrator]
 
     # filtrar os ids dos pedidos (casa)
-    seletor_casa = Seletor(pedidos_casa)
-    ids_casa = seletor_casa.filtrar_ids_casa()
+    ids_casa = Seletor().filtrar_ids_casa(pedidos_casa)
 
     # pegar os pedidos (casa) da api get orders
-    extrator_get_casa = Extrator(url_get_order_casa, headers_casa)
-    pedidos_casa = [extrator_get_casa.baixar_pagina(id_casa) for id_casa in ids_casa]
+    abracasa_get_extrator = Extrator(url_get_order_casa, headers_casa)
+    pedidos_casa = [abracasa_get_extrator.baixar_pagina(id_casa) for id_casa in ids_casa]
 
     return pedidos_cadabra, pedidos_mais, pedidos_casa
--- tests/test_extract/__init__.py
+++ tests/test_extract/__init__.py
@@ -1,9 +1,13 @@
-# import unittest
-# from abrapi.extract import resgatar_pedidos
-#
-# class MyTestCase(unittest.TestCase):
-#     def test_something(self):
-#         self.assertEqual(True, False)
-#
-# if __name__ == '__main__':
-#     unittest.main()
+import unittest
+from abrapi.extract import resgatar_pedidos
+
+
+class VerificarPedidosTest(unittest.TestCase):
+    def test_resgatar_pedidos(self):
+        pedidos_cadabra, pedidos_mais, pedidos_casa = resgatar_pedidos('2020-09-04', '2020-09-05')
+        # não sei testar isso, só joguei um breakpoint
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()

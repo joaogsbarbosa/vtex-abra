5dd85a6968df10b5039ea6745b551d3da8df26ed
João Gabriel <jgsb123@gmail.com>
2020-10-08 22:33:47 -0300

    removendo o flask e arquivos inúteis

--- app.py
+++ app.py
@@ -1,17 +1,4 @@
-from flask import Flask, render_template, request, session, Response
-
 import abrapi
 
-app = Flask(__name__)
-
-app.secret_key = b"1\xfe&\xbdI\nAA\x8c\xdd'M\xfe\xb8r\x10"
-
-abrapi.rodar()
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    abrapi.rodar()

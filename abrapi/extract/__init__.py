bbb276893ebfe21b1b0dde7bfb91601aa64c1f46
João Gabriel <jgsb123@gmail.com>
2020-10-09 18:34:52 -0300

    melhoria nos comentários

--- abrapi/extract/__init__.py
+++ abrapi/extract/__init__.py
@@ -23,29 +23,30 @@ def resgatar_pedidos(data_inicio, data_fim):
     abracasa_get = gerar_get_order('abracasa')
     abracasa_headers = gerar_headers(ABRACASA_APPKEY, ABRACASA_APPTOKEN)
 
-    # Abra mais
+    # Abra Mais
 
-    # pegar todos os pedidos da api de list orders
+    # Pegar todos os pedidos da api de list orders
+
     abramais_list_extrator = ListOrdersExtrator(abramais_list, abramais_headers)
     pedidos_abramais = [pedidos for pedidos in abramais_list_extrator]
 
-    # filtrar os ids dos pedidos
+    # Filtrar os ids dos pedidos
     ids_abramais = Seletor().filtrar_ids(pedidos_abramais)
 
-    # pegar os pedidos na api get orders
+    # Pegar os pedidos na api get orders
     abramais_get_extrator = Extrator(abramais_get, abramais_headers)
     pedidos_abramais = [abramais_get_extrator.baixar_pagina(id_abramais) for id_abramais in ids_abramais]
 
     # Abra Casa
 
-    # pegar todos os pedidos da api de list orders
+    # Pegar todos os pedidos da api de list orders
     abracasa_list_extrator = ListOrdersExtrator(abracasa_list, abracasa_headers)
     pedidos_abracasa = [pedidos for pedidos in abracasa_list_extrator]
 
-    # filtrar os ids dos pedidos (casa)
+    # Filtrar os ids dos pedidos
     ids_abracasa = Seletor().filtrar_ids(pedidos_abracasa)
 
-    # pegar os pedidos (casa) da api get orders
+    # Pegar os pedidos da api get orders
     abracasa_get_extrator = Extrator(abracasa_get, abracasa_headers)
     pedidos_abracasa = [abracasa_get_extrator.baixar_pagina(id_abracasa) for id_abracasa in ids_abracasa]
 

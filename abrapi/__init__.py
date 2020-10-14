3d2c11e90715bad2a7824e646e7086d7c91e9003
João Gabriel <jgsb123@gmail.com>
2020-10-13 21:17:23 -0300

    menu adicionado

--- abrapi/__init__.py
+++ abrapi/__init__.py
@@ -2,10 +2,10 @@ from . import extract, transform, load
 from datetime import datetime
 
 
-def rodar():
-    hoje = datetime.now().strftime("%Y-%m-%d")
+def rodar(data: datetime = datetime.now()):
+    data = data.strftime("%Y-%m-%d")
 
-    pedidos_abramais, pedidos_abracasa = extract.resgatar_pedidos(str(hoje), str(hoje))
+    pedidos_abramais, pedidos_abracasa = extract.resgatar_pedidos(data, data)
 
     abramais_query = transform.transformar_postgresql(pedidos_abramais)
     abracasa_query = transform.transformar_postgresql(pedidos_abracasa)
--- app.py
+++ app.py
@@ -1,4 +1,38 @@
+from datetime import datetime
+from datetime import timedelta
 import abrapi
 
+
+def iniciar(data=datetime.now().date()):
+    if isinstance(data, str):
+        data = datetime.strptime(data, '%Y-%m-%d').date()
+
+    try:
+        while True:
+            print("Resgatando pedidos da data", data.strftime("%Y-%m-%d"))
+            abrapi.rodar(data)
+            if datetime.now().date() > data:
+                data += timedelta(days=1)
+    except KeyboardInterrupt:
+        print('interrupted!')
+
+
+def abrir_menu():
+    print("Escolhas as opções:")
+    print("1 - resgatar pedidos a partir de hoje")
+    print("2 - resgatar pedidos a partir de uma data definida")
+    entrada = int(input())
+    if entrada == 1:
+        iniciar()
+    elif entrada == 2:
+        print("Digite a data inicial")
+        print("ano-mês-dia")
+        print("Ex: 2019-03-28")
+        entrada = str(input())
+        iniciar(entrada)
+    else:
+        abrir_menu()
+
+
 if __name__ == "__main__":
-    abrapi.rodar()
+    abrir_menu()
